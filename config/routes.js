0d332ee6478eab303e88aeb2c8de40489809d886
Rodrigo Monteiro Barboza <rodboza@gmail.com>
2017-07-18 09:19:56 -0300

    Update routes.js

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const auth = require('./auth')
 
+module.export = function (server) {
+
 server.get('/', function(req, res) {
   res.send('<h1>Index!</h1>')
 })
+  
+}
 
