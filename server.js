bd1f3cc05482db6f8dc8a0595b89199cdb589fe5
Rodrigo Monteiro Barboza <rodboza@gmail.com>
2017-07-18 09:18:49 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 console.log("inicio do arquivo /server.js");
-
 const server = require('./config/server')
-
+require('./config/database')
+require('./config/routes')(server)
 console.log("fim do arquivo /server.js");
 
-//require('./config/database')
-//require('./config/routes')(server)
 
 /*
 var http = require('http');
