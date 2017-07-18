e4602ff46a5c0ed569a4ec0c5847f04124a5dbf5
Rodrigo Monteiro Barboza <rodboza@gmail.com>
2017-07-18 16:31:59 -0300

    Update routes.js

--- config/routes.js
+++ config/routes.js
@@ -3,6 +3,18 @@ const auth = require('./auth');
 
 module.exports = function (server) {
 
+	/*
+	 * Rotas abertas
+	 */
+	const openApi = express.Router();
+	server.use('/auth', openApi);
+
+	const AuthService = require('../api/user/authService');
+	openApi.post('/login', AuthService.login);
+	openApi.post('/signup', AuthService.signup);
+	openApi.post('/validateToken', AuthService.validateToken)  ;
+  
+  
   server.use(auth);
   
   server.get('/', function(req, res) {
