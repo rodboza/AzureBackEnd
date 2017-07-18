1bed0fe890881e6ecd5fd8de4d0b2df89d4fb8e6
Rodrigo Monteiro Barboza <rodboza@gmail.com>
2017-07-18 08:50:58 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,4 +7,5 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('Hello, world! [helloworld sample; iisnode version is ' + process.env.IISNODE_VERSION + ', node version is ' + process.version + ']');
+    console.log("teste");
 }).listen(process.env.PORT);  
