a0573c6ab4f06bee9f2b17ed8be59b9ec16e20a9
Rodrigo Monteiro Barboza <rodboza@gmail.com>
2017-07-18 11:15:18 -0300

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -1,30 +1,30 @@
-const jwt = require('jsonwebtoken')
-const env = require('../.env')
+const jwt = require('jsonwebtoken');
+const env = require('../.env');
 
 module.exports = (req, res, next) => {
 
-    consult.log('token');
+    console.log('token');
     // CORS preflight request
     if (req.method === 'OPTIONS') {
-        next()
+        next();
     } else {
-        const token = req.body.token || req.query.token || req.headers['authorization']
+        const token = req.body.token || req.query.token || req.headers['authorization'];
 
-        consult.log(token);
-        consult.log(env._authSecret);
+        console.log(token);
+        console.log(env._authSecret);
         
         if (!token) {
-            return res.status(403).send({ errors: ['No token provided.'] })
+            return res.status(403).send({ errors: ['No token provided.'] });
         }
 
         jwt.verify(token, env._authSecret, function (err, decoded) {
             if (err) {
                 return res.status(403).send({
-                    errors: ['Failed to authenticate token.']
+                    errors: ['Failed to authenticate token.'];
                 })
             } else {
-                req.decoded = decoded
-                next()
+                req.decoded = decoded;
+                next();
             }
         })
     }
