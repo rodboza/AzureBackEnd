6cb7b011d5d4c375297aa815397a0465e6a2e5d2
Rodrigo Monteiro Barboza <rodboza@gmail.com>
2017-07-26 09:29:53 -0300

    Update auth.js

--- app/api/auth.js
+++ app/api/auth.js
@@ -15,28 +15,35 @@ module.exports = function(app) {
 	const sendErrorsFromDB = (res, dbErrors) => {
 		const errors = [];
 		_.forIn(dbErrors.errors, error => errors.push(error.message));
-		return res.status(400).json({ errors });
+		if (res.status == -1)
+			res.status(400);
+		return res.json({ errors });
+	}
+	
+	const enviaErro = ( _res, _status, _msg ) => {
+		_res.status(_status);
+		throw new Error(_msg);
 	}
 
 	api.login = function(req, res, next) {
 		const email = req.body.email || '';
 		const password = req.body.password || '';
 
-		User.findOne({ email }, (err, user) => {
-			if (err) {
-				return sendErrorsFromDB(res, err);
-			} else if (user && bcrypt.compareSync(password, user.password)) {
-				const token = jwt.sign(user, env._authSecret, {
-					expiresIn: "1 day"
-				});
-				const { name, email } = user;
-				//res.json({ name, email, token });
-				return res.status(200).send({ name, email, token });
-			} else {
-				return res.status(400).send({ errors: ['Usuário/Senha inválidos'] });
-			}
-		})
+		User.findOne({ email } )
+			.then( 
+				function (user){
+					if (! ( user && bcrypt.compareSync(password, user.password ) ) ) 
+						enviaErro(res,404, 'Usuário/Senha inválidos');
+					const token = jwt.sign(user, env._authSecret, {expiresIn: "1 day"});
+					const { name, email } = user;
+					return res.status(200).send({ name, email, token });
+				}
+				,function ( err ) {
+					return sendErrorsFromDB(res, err);
+				}
+			);
 	}
+
 
 	api.logout = function(req, res, next) {
 		res.end("<h1>logout</h1>");
@@ -49,41 +56,38 @@ module.exports = function(app) {
 		const password = req.body.password || '';
 		const confirmPassword = req.body.confirm_password || '';
 
+		User.findOne({ email })
+			.then(
+				function (user){
+					if (user) 
+						return enviaErro ( res, 400, env._usuarioJaCadastrado );
+					
+					if (!email.match(env._emailRegex))
+						return enviaErro ( res, 400, env._emailInvalido );
+					
+					if (!password.match(env._passwordRegex))
+						return enviaErro ( res, 400, env._senhaInvalida );
 
-		if (!email.match(env._emailRegex)) {
-			return res.status(400).send({ errors: [ env._emailInvalido ] });
-		}
-
-		if (!password.match(env._passwordRegex)) {
-			return res.status(400).send({ errors: [ env._senhaInvalida ] });
-		}
-
-		const salt = bcrypt.genSaltSync();
-		const passwordHash = bcrypt.hashSync(password, salt);
-		if (!bcrypt.compareSync(confirmPassword, passwordHash)) {
-			return res.status(400).send({ errors: [ env._senhaDifConfirmacao ] });
-		}
-
-		User.findOne({ email }, (err, user) => {
-			if (err) {
-				return sendErrorsFromDB(res, err);
-			} else if (user) {
-				return res.status(400).send({ errors: [ env._usuarioJaCadastrado ] });
-			} else {
-				const newUser = new User({ name, email, password: passwordHash });
-				newUser.save(err => {
-					if (err) {
-						return sendErrorsFromDB(res, err);
-					} else {
-						//api.login(req, res, next);
-						next();
-					}
-				})
-			}
-		})
-
-
+					const salt = bcrypt.genSaltSync();
+					const passwordHash = bcrypt.hashSync(password, salt);
+					if (!bcrypt.compareSync(confirmPassword, passwordHash))
+						return enviaErro ( res, 400, env._senhaDifConfirmacao );
+					
+					const newUser = new User({ name, email, password: passwordHash });
+					newUser.save()
+						.then( 
+							function (err){
+								return sendErrorsFromDB(res, err);
+							}
+						);
+					next();
+				}
+				,function (err){
+					return sendErrorsFromDB(res, err);
+				}
+			)
 	}
+
 
 	api.validarToken = function(req, res, next) {
 		console.log("token validado");
